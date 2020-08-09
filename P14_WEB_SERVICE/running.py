bfbad6875968b317a11576d30a0e408f58dcd0e2
malith98 <60734464+malith98@users.noreply.github.com>
2020-08-09 15:39:20 +0300

    Add files via upload

---
+++ P14_WEB_SERVICE/running.py
@@ -0,0 +1,14 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/malit/OneDrive/Desktop/computer studies/P14_WEB_SERVICE/P14_Web_Service.py
+serving at port 8000
+Traceback (most recent call last):
+  File "C:/Users/malit/OneDrive/Desktop/computer studies/P14_WEB_SERVICE/P14_Web_Service.py", line 8, in <module>
+    httpd.server_forever()
+AttributeError: 'TCPServer' object has no attribute 'server_forever'
+>>> 
+======== RESTART: C:/Users/malit/OneDrive/Desktop/computer studies/xx.py =======
+Server running on port 8000
+127.0.0.1 - - [09/Aug/2020 14:47:01] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [09/Aug/2020 14:47:01] "GET /favicon.ico HTTP/1.1" 200 -
